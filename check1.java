9a4f61b9b1db7f45be1c551941ba884ffe9dcb90
shubham-programmerr <dadhichshubham32@gmail.com>
2025-03-22 11:43:17 +0530

    22/03/2025

---
+++ check1.java
@@ -0,0 +1,9 @@
+public class check1{
+    public static void main(String[] args)
+    {
+        var x = 100;
+        var z ='a';
+        System.out.println(x);
+        System.out.println(z);
+    }
+}
